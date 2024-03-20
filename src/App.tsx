29b532f96d06740d2a77035032e9f984c9380dcd
Michal Podolak <60146710+myky123@users.noreply.github.com>
2024-03-20 07:31:33 +0000

    feat: fetch card data Create drawCardHandler function Create appropriate states

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,46 @@ import { useEffect, useState } from "react";
 import { Header } from "./components/Header/Header";
 
 function App() {
+    const [card1, setCard1] = useState();
+    const [card2, setCard2] = useState();
     const [deckId, setDeckId] = useState("");
-    const [error, setError] = useState(null);
+    const [error, setError] = useState("");
+    const [cardsLeft, setCardsLeft] = useState(52);
     const [isLoading, setIsLoading] = useState(false);
+
+    const drawCardHandler = async () => {
+        try {
+            setIsLoading(true);
+
+            const drawCardUrl = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+
+            const response = await fetch(drawCardUrl);
+            if (!response?.ok) {
+                throw Error("Couldn't fetch data");
+            }
+
+            const card = await response.json();
+
+            setIsLoading(false);
+            setCardsLeft(card.remaining);
+            setCard1(card);
+            setError("");
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
+        }
+    };
 
     const fetchDeckHandler = async (numberOfDecks: number) => {
         try {
-            const drawDeckUrl = `https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${numberOfDecks}`;
+            const drawDeckUrl = `https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_counT=${numberOfDecks}`;
 
             const response = await fetch(drawDeckUrl);
+
+            if (!response?.ok) {
+                throw Error("Couldn't fetch data");
+            }
 
             const deck = await response.json();
 
@@ -20,11 +51,13 @@ function App() {
             setDeckId(deck_id);
 
             // reset error
-            setError(null);
+            setError("");
 
             setIsLoading(false);
-        } catch (error: any) {
-            setError(error?.message);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                setError(error.message);
+            }
         }
     };
 
